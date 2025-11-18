dd6c73880017aed59f862e6ff9738b0d921dcfd4
yuliia <yuliiadev@ukr.net>
2025-11-18 19:47:31 +0200

    third

--- index.php
+++ index.php
@@ -2,3 +2,9 @@
 echo "Привіт! Docker + PHP працює 🎉<br>";
 echo "Час оновлення: " . date('H:i:s') . "<br>";
 echo "Це новий рядок для перевірки!";
+echo "How was your day";
+echo "it was nice";
+echo "thank you";
+echo "How was your day";
+echo "it was nice";
+echo "thank you";
