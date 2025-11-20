c772c70d47a932d26d68738626353042053b5952
yuliia <yuliiadev@ukr.net>
2025-11-20 23:34:13 +0200

    117

--- index3.php
+++ index3.php
@@ -3,9 +3,9 @@
 <?php
 //seconds in hour
 $sec_in_hour = 60 * 60;
-echo "seconds in hour" . $sec_in_hour;
-
+echo "seconds in hour" . $sec_in_hour; ?>
+<?php
 $a = 10;
 $b = 2;
 $sum_a_b = $a * $b;
-echo $sum_a_b;
+echo $sum_a_b; ?>
