e4bf715f4edfcd7be96baec77dd201ec0b6a14d7
yuliia <yuliiadev@ukr.net>
2025-12-14 01:38:11 +0200

    144

--- index5.php
+++ index5.php
@@ -3,5 +3,19 @@ $arr = ['Привет, ', 'мир', '!'];
 
 $newArr = implode('', $arr);
 
-print_r ($newArr);
+print_r($newArr);
+?>
+<hr>
+
+<?php
+
+$salary = [
+
+    'Petya' => 1000,
+    'kolya' => 4000,
+    'Olha' => 7000
+];
+
+print_r('Petyas salary' . $salary['Petya']);
+
 ?>
