9757388b265af211f2d03e05704547cdfe202148
yuliia <yuliiadev@ukr.net>
2025-12-21 23:12:11 +0200

    165

--- index7.php
+++ index7.php
@@ -14,8 +14,9 @@ echo concat(2, 5);
 
 function rgb($num1 = 0, $num2 = 0, $num3 = 0)
 {
-    return rgb($num1, $num2, $num3);
+    return "rgb($num1, $num2, $num3)";
 }
+
 echo rgb(12, 34, 71);
 
 ?>
