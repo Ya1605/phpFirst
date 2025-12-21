9757628275576d6987df55cd42e12e88de3c0d3e
yuliia <yuliiadev@ukr.net>
2025-12-21 23:06:42 +0200

    162

---
+++ index7.php
@@ -0,0 +1,22 @@
+
+<?php
+
+use BcMath\Number;
+
+function concat($numb, $value){
+    return pow($numb, $value);
+}
+echo concat(2,5);
+?>
+<hr>
+<?php 
+
+function rgb(number $num1=0, number $num2=0, number $num3=0)
+{
+return rgb($num1, $num2, $num3);
+
+}
+echo rgb();
+
+?>
+
