15de228a8baf40690d31d176b8a7db3439eecd44
yuliia <yuliiadev@ukr.net>
2025-12-29 12:33:18 +0200

    173

--- index8.php
+++ index8.php
@@ -2,5 +2,18 @@
 $randNumb = random_int(1, 30);
 echo $randNumb;
 
+?>
 
+<?php
+function randNumber($length)
+{
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $result = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $chars[rand(0, strlen($chars) - 1)];
+    }
+
+    return $result;
+}
 ?>
