e7691aa935146fb88e50042e78e22a7e70be36e3
yuliia <yuliiadev@ukr.net>
2026-01-05 22:45:32 +0200

    188

--- index9.php
+++ index9.php
@@ -1,11 +1,44 @@
 <?php
-class HelloWorld{
+class HelloWorld
+{
 
-    public function echoMessage($message){
+    public function echoMessage($message)
+    {
         return $message;
     }
 }
 
 $firstmes = new HelloWorld;
 echo $firstmes->echoMessage("hello world");
+?>
+
+<?php
+class Calculator
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b)
+    {
+        return $a - $b;
+    }
+
+    public function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+    public function divide($a, $b)
+    {
+        if ($b == 0) {
+            return "Ошибка: деление на ноль!";
+        }
+        return $a / $b;
+    }
+}
+$mycalculate = new Calculator;
+echo $mycalculate->add(5, 4);
+echo $mycalculate->divide(10, 20);
+
 ?>
